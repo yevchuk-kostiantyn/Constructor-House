02c31e032034cf8379d92bf668502cf30fbfbcd9
Kostiantyn Yevchuk <kostyantyn.yewchuk@gmail.com>
2017-04-14 14:32:32 +0300

    Comment HTML, CSS and JS files

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
     function show(container, Message) {
-      container.className = 'message'; //error
+      container.className = 'message';
       var msgElem = document.createElement('span');
       msgElem.className = "output-message";
       msgElem.innerHTML = Message;
       container.appendChild(msgElem);
     }
 
-    function showMessage(container) { 
+    function showMessage(container) {
       container.className = '';
       if (container.lastChild.className == "output-message") {
         container.removeChild(container.lastChild);
@@ -19,20 +19,32 @@
       // Calculate Price
       showMessage(house.area.parentNode);
       if (house.material.value == 'bricks') {
-      	show(house.price.parentNode, house.area.value * 1500 + '$')
-      	document.getElementById("myBtn").disabled = true;
+      	show(house.price.parentNode, house.area.value * 1200 + '$')
+      	document.getElementById("Build_button").disabled = true;
       }
       else if(house.material.value == 'wood') {
-      	show(house.price.parentNode, house.area.value * 1000 + '$')
-      	document.getElementById("myBtn").disabled = true;
+      	show(house.price.parentNode, house.area.value * 1500 + '$')
+      	document.getElementById("Build_button").disabled = true;
       }
       else if(house.material.value == 'metal') {
-      	show(house.price.parentNode, house.area.value * 1200 + '$')
-      	document.getElementById("myBtn").disabled = true;
+      	show(house.price.parentNode, house.area.value * 1000 + '$')
+      	document.getElementById("Build_button").disabled = true;
       }
+      else if(house.material.value == 'foam blocks') {
+        show(house.price.parentNode, house.area.value * 1300 + '$')
+        document.getElementById("Build_button").disabled = true;
+      }
+      else if(house.material.value == 'wireframes') {
+        show(house.price.parentNode, house.area.value * 800 + '$')
+        document.getElementById("Build_button").disabled = true;
+      }
+      
       else {
       	show(house.price.parentNode, 'Sorry, we do not build with this material')
-      	document.getElementById("myBtn").disabled = true;
+      	document.getElementById("Build_button").disabled = true;
       }
     }
+
+    // foam blocks
+    // wireframes
     
